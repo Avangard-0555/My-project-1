f96dd531ab32603683a78bf57a972aee47f8064c
Avangard-0555 <turgunovx98@gmail.com>
2025-02-10 22:36:26 +0500

    config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+OPENAI_API_KEY = "your_openai_api_key"
+GOOGLE_APPLICATION_CREDENTIALS = "your_google_cloud_credentials.json"
+
+#export GOOGLE_APPLICATION_CREDENTIALS="your_google_cloud_credentials.json"
